f91a4c94c4d98009bc95333535420d1f6b798ce6
Lucy de Rojas <lucyderojas@gmail.com>
2024-12-22 23:18:30 +0000

    drop and read working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,114 +1,96 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { useState, useEffect } from "react";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  console.clear();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const [audioData, setAudioData] = useState(null);
+
+
+
+
+
+  function handleOnDragEnter(e) {}
+  function handleOnDragOver(e) {e.preventDefault();}
+  function handleOnDragLeave(e) {}
+
+
+
+  function handleOnDrop(e) {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+
+    if (files.length > 0) {
+      const file = files[0];
+      const reader = new FileReader();
+      reader.onload = function(event) {
+      const arrayBuffer = event.target.result;
+      const dataView = new DataView(arrayBuffer);
+
+      const header = {
+        chunkId: String.fromCharCode(dataView.getUint8(0)) + String.fromCharCode(dataView.getUint8(1)) + String.fromCharCode(dataView.getUint8(2)) + String.fromCharCode(dataView.getUint8(3)),
+        chunkSize: dataView.getUint32(4, true),
+        format: String.fromCharCode(dataView.getUint8(8)) + String.fromCharCode(dataView.getUint8(9)) + String.fromCharCode(dataView.getUint8(10)) + String.fromCharCode(dataView.getUint8(11)),
+        subChunk1Id: String.fromCharCode(dataView.getUint8(12)) + String.fromCharCode(dataView.getUint8(13)) + String.fromCharCode(dataView.getUint8(14)) + String.fromCharCode(dataView.getUint8(15)),
+        subChunk1Size: dataView.getUint32(16, true),
+        audioFormat: dataView.getUint16(20, true),
+        numChannels: dataView.getUint16(22, true),
+        sampleRate: dataView.getUint32(24, true),
+        byteRate: dataView.getUint32(28, true),
+        blockAlign: dataView.getUint16(32, true),
+        bitsPerSample: dataView.getUint16(34, true),
+        subChunk2Id: String.fromCharCode(dataView.getUint8(36)) + String.fromCharCode(dataView.getUint8(37)) + String.fromCharCode(dataView.getUint8(38)) + String.fromCharCode(dataView.getUint8(39)),
+        subChunk2Size: dataView.getUint32(40, true),
+      };
+
+      console.log(header);
+      setAudioData(header);
+      };
+      reader.readAsArrayBuffer(file);
+    }
+
+  }
+
+
+
+
+
+  return (<div className="bg-green-200">
+    <h1>Wav Parser</h1>
+
+
+    <div className="flex flex-col items-center justify-center h-[500] bg-gray-100"
+    onDrop={handleOnDrop}
+    onDragOver={handleOnDragOver}
+
+
+    >
+
+  drop files here
+    </div>
+{
+audioData && <div>Result:
+
+<p>chunk Id: {audioData.chunkId}</p>
+<p>chunk Size: {audioData.chunkSize}</p>
+<p>chunk Format: {audioData.format}</p>
+<p>chunk Sub Chunk ID: {audioData.subChunk1Id}</p>
+<p>chunk Audio Format: {audioData.audioFormat}</p>
+<p>chunk Num Channels: {audioData.numChannels}</p>
+<p>chunk Sample Rate: {audioData.sampleRate}</p>
+<p>chunk Byte Rate: BYTE {audioData.byteRate}</p>
+<p>chunk Block Align: {audioData.blockAlign}</p>
+<p>chunk Bits Per Sample: {audioData.bitsPerSample}</p>
+
+
+
+</div>
+}
+
+
+
     </div>
   );
 }
